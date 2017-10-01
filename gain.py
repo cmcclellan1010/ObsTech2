908e899a919934ad687114945e20287874b6cc4d
cmcclellan1010 <cmcclellan1010@ufl.edu>
2017-10-01 15:56:22 -0400

    first commit

---
+++ gain.py
@@ -0,0 +1,103 @@
+from astropy.io import fits
+from scipy.stats import norm
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+
+
+def construct_data_array(filename_prefix, n_images):
+    da = []
+    for i in range(n_images):
+        if i+1 < 10:
+            j = '00'+str(i+1)
+        else:
+            j = '0'+str(i+1)
+        da.append(fits.open(filename_prefix+j+'.FIT')[0].data)
+    da = np.array(da, dtype=float)
+    shape = np.shape(da)
+    print "Array shape: ", shape
+    n_pixels = shape[1] * shape[2]
+    print "Number of pixels: ", n_pixels
+    return da
+
+### EXPERIMENTAL SECTION ###
+testdata = construct_data_array('flat_1e5ms_', 3)
+master_dark_1e5ms = np.median(construct_data_array('dark_1e5ms_', 3), axis=0)
+testdata -= master_dark_1e5ms
+average_of_square = np.mean(testdata ** 2, axis=0)
+print average_of_square
+square_of_average = (np.mean(testdata, axis=0)) ** 2
+print square_of_average
+variance = np.absolute(average_of_square - square_of_average)
+print variance
+
+prefix_list = ['flat_1e4ms_', 'flat_1e5ms_', 'flat_2e5ms_', 'flat_3e4ms_', 'flat_6e4ms_', 'flat_12e4ms_']
+y = np.ndarray((len(prefix_list), 390150))
+x = np.ndarray((len(prefix_list), 390150))
+
+for prefix in prefix_list:
+    i = prefix_list.index(prefix)
+
+    # Calculate mean array
+    data = construct_data_array(prefix, 3)
+    mean_array = np.mean(data, axis=0)
+
+    # READ NOISE 1, 2: Calculate RMS array
+    average_of_square = np.mean(data**2, axis=0)
+    print average_of_square
+    square_of_average = (np.mean(data, axis=0))**2
+    print square_of_average
+    variance = np.absolute(average_of_square - square_of_average)
+    print variance
+
+# Plot variance vs signal
+    y[i] = np.ndarray.flatten(variance)
+    x[i] = np.ndarray.flatten(mean_array)
+
+
+fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(17, 11))
+
+ax1.plot(x[0], y[0], 'bo')
+ax1.set_title('1e4 ms')
+ax1.set_xlabel('Signal (e-)')
+ax1.set_ylabel('Variance (RMS^2)')
+
+ax2.plot(x[3], y[3], 'bo')
+ax2.set_title('3e4 ms')
+ax2.set_xlabel('Signal (e-)')
+ax2.set_ylabel('Variance (RMS^2)')
+
+ax3.plot(x[4], y[4], 'bo')
+ax3.set_title('6e4 ms')
+ax3.set_xlabel('Signal (e-)')
+ax3.set_ylabel('Variance (RMS^2)')
+
+ax4.plot(x[1], y[1], 'bo')
+ax4.set_title('1e5 ms')
+ax4.set_xlabel('Signal (e-)')
+ax4.set_ylabel('Variance (RMS^2)')
+
+ax5.plot(x[5], y[5], 'bo')
+ax5.set_title('12e4 ms')
+ax5.set_xlabel('Signal (e-)')
+ax5.set_ylabel('Variance (RMS^2)')
+
+ax6.plot(x[2], y[2], 'bo')
+ax6.set_title('2e5 ms')
+ax6.set_xlabel('Signal (e-)')
+ax6.set_ylabel('Variance (RMS^2)')
+
+plt.tight_layout()
+plt.show()
+
+
+x_total = np.ndarray.flatten(x)
+y_total = np.ndarray.flatten(y)
+print y[0]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+plt.plot(x_total, y_total, 'bo')
+plt.xlabel('Signal (e-)')
+plt.ylabel('Variance (RMS^2)')
+plt.show()
---
+++ readnoise.py
@@ -0,0 +1,168 @@
+from astropy.io import fits
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.modeling import models, fitting
+
+
+def rms(x):
+    return (np.absolute(np.mean(x**2) - (np.mean(x))**2))**0.5
+
+
+def construct_data_array(filename_prefix, n_images):
+    da = []
+    for i in range(n_images):
+        if i+1 < 10:
+            j = '00'+str(i+1)
+        else:
+            j = '0'+str(i+1)
+        da.append(fits.open(filename_prefix+j+'.FIT')[0].data)
+    da = np.array(da, dtype=float)
+    shape = np.shape(da)
+    n_pixels = shape[1] * shape[2]
+    return da, n_pixels
+
+
+# READ NOISE 1, 2: Calculate RMS array
+
+data_100ms = construct_data_array('dark_100ms_', 18)
+data_200ms = construct_data_array('dark_1000ms_', 6)
+data_1000ms = construct_data_array('dark_1e4ms_', 6)
+data_1e4ms = construct_data_array('dark_1e5ms_', 3)
+
+data_list = [data_100ms, data_200ms, data_1000ms, data_1e4ms]
+
+final_RMS_array = []
+for i in range(4):
+    average_of_square = np.mean(data_list[i][0]**2, axis=0)
+    square_of_average = (np.mean(data_list[i][0], axis=0))**2
+    variance = average_of_square - square_of_average
+    RMS = np.absolute(variance)**0.5
+
+    # READ NOISE 3: Find mean, median, and RMS of RMS array
+    npix = data_list[i][1]
+    print "i = ", i
+    print "Mean: ", np.mean(RMS)
+    print "Median: ", np.median(RMS)
+    print "RMS: ", rms(RMS)
+    print "Read noise uncertainty: ", rms(RMS)/np.sqrt(npix), " per pixel"
+
+    # READ NOISE 4: Make a histogram of RMS array values
+    final_RMS_array.append(np.ndarray.flatten(RMS))
+
+
+print "\nGauss Fit measurements:"
+fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, figsize=(12, 12))
+
+# PLOT #1
+n, bins, patches = ax1.hist(final_RMS_array[0], 100, range=[0, 30])
+bincenters = 0.5*(bins[1:]+bins[:-1])
+
+g_init = models.Gaussian1D(amplitude=18000, mean=12, stddev=2.5)
+fit_g = fitting.LevMarLSQFitter()
+g = fit_g(g_init, bincenters, n)
+
+mean1, stddev1 = g.mean[0], g.stddev[0]
+print 'i = 1'
+print "Mean: ", mean1
+print "Stddev: ", stddev1
+
+ax1.plot(bincenters, g(bincenters), 'r--', linewidth=3)
+ax1.set_xlabel('Pixel-wise RMS of DNs')
+ax1.set_ylabel('N')
+ax1.set_xlim(0, 30)
+ax1.set_ylim(0, 20000)
+ax1.grid(True)
+ax1.set_title('100ms Dark')
+
+
+# PLOT #2
+n, bins, patches = ax2.hist(final_RMS_array[1], 100, range=[0, 30])
+bincenters = 0.5*(bins[1:]+bins[:-1])
+
+g_init = models.Gaussian1D(amplitude=12500, mean=10.6, stddev=4)
+fit_g = fitting.LevMarLSQFitter()
+g = fit_g(g_init, bincenters, n)
+
+mean2, stddev2 = g.mean[0], g.stddev[0]
+print 'i = 2'
+print "Mean: ", mean2
+print "Stddev: ", stddev2
+
+ax2.plot(bincenters, g(bincenters), 'r--', linewidth=3)
+ax2.set_xlabel('Pixel-wise RMS of DNs')
+ax2.set_ylabel('N')
+ax2.set_xlim(0, 30)
+ax2.set_ylim(0, 20000)
+ax2.grid(True)
+ax2.set_title('1s Dark')
+
+
+# PLOT 3
+n, bins, patches = ax3.hist(final_RMS_array[2], 100, range=[0, 30])
+bincenters = 0.5*(bins[1:]+bins[:-1])
+
+g_init = models.Gaussian1D(amplitude=12500, mean=10.6, stddev=4)
+fit_g = fitting.LevMarLSQFitter()
+g = fit_g(g_init, bincenters, n)
+
+mean3, stddev3 = g.mean[0], g.stddev[0]
+print 'i = 3'
+print "Mean: ", mean3
+print "Stddev: ", stddev3
+
+ax3.plot(bincenters, g(bincenters), 'r--', linewidth=3)
+ax3.set_xlabel('Pixel-wise RMS of DNs')
+ax3.set_ylabel('N')
+ax3.set_xlim(0, 30)
+ax3.set_ylim(0, 20000)
+ax3.grid(True)
+ax3.set_title('10s Dark')
+
+
+# PLOT 4
+n, bins, patches = ax4.hist(final_RMS_array[3], 100, range=[0, 30])
+bincenters = 0.5*(bins[1:]+bins[:-1])
+
+g_init = models.Gaussian1D(amplitude=10000, mean=8.9, stddev=5.6)
+fit_g = fitting.LevMarLSQFitter()
+g = fit_g(g_init, bincenters, n)
+
+mean4, stddev4 = g.mean[0], g.stddev[0]
+print 'i = 4'
+print "Mean: ", mean4
+print "Stddev: ", stddev4
+
+ax4.plot(bincenters, g(bincenters), 'r--', linewidth=3)
+ax4.set_xlabel('Pixel-wise RMS of DNs')
+ax4.set_ylabel('N')
+ax4.set_xlim(0, 30)
+ax4.set_ylim(0, 20000)
+ax4.grid(True)
+ax4.set_title('100s Dark')
+
+plt.tight_layout()
+plt.show()
+
+exptime = [100, 1000, 10000, 100000]
+means = [mean1, mean2, mean3, mean4]
+err = [stddev1, stddev2, stddev3, stddev4]
+
+l_init = models.Linear1D(slope=0, intercept=956)
+fit_l = fitting.LinearLSQFitter()
+l = fit_l(l_init, exptime, means)
+slope, intercept = l.slope[0], l.intercept[0]
+print "Slope, Intercept: ", slope, intercept
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.annotate('Slope: '+str("%.2E" % slope), xy=(20000, 11))
+ax.annotate('Y-Intercept: '+str("%.2f" % intercept), xy=(20000, 10.5))
+plt.errorbar(exptime, means, yerr=err, fmt='ko', label='Mean Read Noise (stddev in error bars)')
+plt.plot(exptime, l(exptime), 'limegreen', label='Linear fit')
+plt.xlabel('Exposure time (ms)')
+plt.ylabel('Mean Read Noise (DNs)')
+plt.xlim(-10000, 110000)
+plt.tight_layout()
+plt.legend(loc=3)
+plt.grid(True)
+plt.show()
