6409f24343e266c04e78ef27b3e2f53fdef8bd0d
cmcclellan1010 <cmcclellan1010@ufl.edu>
2017-10-01 16:12:41 -0400

    tested dark subtraction of flats

--- gain.py
+++ gain.py
@@ -21,83 +21,44 @@ def construct_data_array(filename_prefix, n_images):
     return da
 
 ### EXPERIMENTAL SECTION ###
-testdata = construct_data_array('flat_1e5ms_', 3)
+flat_1e4 = construct_data_array('flat_1e4ms_', 3)
+signal_1e4 = np.mean(flat_1e4, axis=0)
+master_dark_1e4ms = np.median(construct_data_array('dark_1e4ms_', 3), axis=0)
+flat_1e4 -= master_dark_1e4ms
+average_of_square = np.mean(flat_1e4 ** 2, axis=0)
+square_of_average = (np.mean(flat_1e4, axis=0)) ** 2
+variance_1e4 = np.absolute(average_of_square - square_of_average)
+
+
+flat_1e5 = construct_data_array('flat_1e5ms_', 3)
+signal_1e5 = np.mean(flat_1e5, axis=0)
 master_dark_1e5ms = np.median(construct_data_array('dark_1e5ms_', 3), axis=0)
-testdata -= master_dark_1e5ms
-average_of_square = np.mean(testdata ** 2, axis=0)
-print average_of_square
-square_of_average = (np.mean(testdata, axis=0)) ** 2
-print square_of_average
-variance = np.absolute(average_of_square - square_of_average)
-print variance
+flat_1e5 -= master_dark_1e5ms
+average_of_square = np.mean(flat_1e5 ** 2, axis=0)
+square_of_average = (np.mean(flat_1e5, axis=0)) ** 2
+variance_1e5 = np.absolute(average_of_square - square_of_average)
 
-prefix_list = ['flat_1e4ms_', 'flat_1e5ms_', 'flat_2e5ms_', 'flat_3e4ms_', 'flat_6e4ms_', 'flat_12e4ms_']
-y = np.ndarray((len(prefix_list), 390150))
-x = np.ndarray((len(prefix_list), 390150))
+signal = np.asarray([signal_1e4, signal_1e5])
+variance = np.asarray([variance_1e4, variance_1e5])
 
-for prefix in prefix_list:
-    i = prefix_list.index(prefix)
+total_signal = np.ndarray.flatten(signal)
+total_variance = np.ndarray.flatten(variance)
 
-    # Calculate mean array
-    data = construct_data_array(prefix, 3)
-    mean_array = np.mean(data, axis=0)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 11))
 
-    # READ NOISE 1, 2: Calculate RMS array
-    average_of_square = np.mean(data**2, axis=0)
-    print average_of_square
-    square_of_average = (np.mean(data, axis=0))**2
-    print square_of_average
-    variance = np.absolute(average_of_square - square_of_average)
-    print variance
-
-# Plot variance vs signal
-    y[i] = np.ndarray.flatten(variance)
-    x[i] = np.ndarray.flatten(mean_array)
-
-
-fig, ((ax1, ax2, ax3), (ax4, ax5, ax6)) = plt.subplots(2, 3, figsize=(17, 11))
-
-ax1.plot(x[0], y[0], 'bo')
+ax1.plot(signal_1e4, variance_1e4, 'bo')
 ax1.set_title('1e4 ms')
 ax1.set_xlabel('Signal (e-)')
 ax1.set_ylabel('Variance (RMS^2)')
 
-ax2.plot(x[3], y[3], 'bo')
+ax2.plot(signal_1e4, variance_1e5, 'bo')
 ax2.set_title('3e4 ms')
 ax2.set_xlabel('Signal (e-)')
 ax2.set_ylabel('Variance (RMS^2)')
 
-ax3.plot(x[4], y[4], 'bo')
-ax3.set_title('6e4 ms')
-ax3.set_xlabel('Signal (e-)')
-ax3.set_ylabel('Variance (RMS^2)')
-
-ax4.plot(x[1], y[1], 'bo')
-ax4.set_title('1e5 ms')
-ax4.set_xlabel('Signal (e-)')
-ax4.set_ylabel('Variance (RMS^2)')
-
-ax5.plot(x[5], y[5], 'bo')
-ax5.set_title('12e4 ms')
-ax5.set_xlabel('Signal (e-)')
-ax5.set_ylabel('Variance (RMS^2)')
-
-ax6.plot(x[2], y[2], 'bo')
-ax6.set_title('2e5 ms')
-ax6.set_xlabel('Signal (e-)')
-ax6.set_ylabel('Variance (RMS^2)')
-
-plt.tight_layout()
-plt.show()
-
-
-x_total = np.ndarray.flatten(x)
-y_total = np.ndarray.flatten(y)
-print y[0]
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
-plt.plot(x_total, y_total, 'bo')
+plt.plot(total_signal, total_variance, 'bo')
 plt.xlabel('Signal (e-)')
 plt.ylabel('Variance (RMS^2)')
 plt.show()
