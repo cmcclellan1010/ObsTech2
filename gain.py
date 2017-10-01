5d753ca2fe5e8716c96e855dfc6b52e7563f62e9
cmcclellan1010 <cmcclellan1010@ufl.edu>
2017-10-01 16:40:12 -0400

    fixed gain issue

--- gain.py
+++ gain.py
@@ -1,8 +1,7 @@
 from astropy.io import fits
-from scipy.stats import norm
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.mlab as mlab
+from astropy.modeling import models, fitting
 
 
 def construct_data_array(filename_prefix, n_images):
@@ -22,21 +21,21 @@ def construct_data_array(filename_prefix, n_images):
 
 ### EXPERIMENTAL SECTION ###
 flat_1e4 = construct_data_array('flat_1e4ms_', 3)
-signal_1e4 = np.mean(flat_1e4, axis=0)
+signal_1e4 = np.mean(np.mean(flat_1e4, axis=0))
 master_dark_1e4ms = np.median(construct_data_array('dark_1e4ms_', 3), axis=0)
 flat_1e4 -= master_dark_1e4ms
 average_of_square = np.mean(flat_1e4 ** 2, axis=0)
 square_of_average = (np.mean(flat_1e4, axis=0)) ** 2
-variance_1e4 = np.absolute(average_of_square - square_of_average)
+variance_1e4 = np.mean(np.absolute(average_of_square - square_of_average))
 
 
 flat_1e5 = construct_data_array('flat_1e5ms_', 3)
-signal_1e5 = np.mean(flat_1e5, axis=0)
+signal_1e5 = np.mean(np.mean(flat_1e5, axis=0))
 master_dark_1e5ms = np.median(construct_data_array('dark_1e5ms_', 3), axis=0)
 flat_1e5 -= master_dark_1e5ms
 average_of_square = np.mean(flat_1e5 ** 2, axis=0)
 square_of_average = (np.mean(flat_1e5, axis=0)) ** 2
-variance_1e5 = np.absolute(average_of_square - square_of_average)
+variance_1e5 = np.mean(np.absolute(average_of_square - square_of_average))
 
 signal = np.asarray([signal_1e4, signal_1e5])
 variance = np.asarray([variance_1e4, variance_1e5])
@@ -44,21 +43,18 @@ variance = np.asarray([variance_1e4, variance_1e5])
 total_signal = np.ndarray.flatten(signal)
 total_variance = np.ndarray.flatten(variance)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(17, 11))
-
-ax1.plot(signal_1e4, variance_1e4, 'bo')
-ax1.set_title('1e4 ms')
-ax1.set_xlabel('Signal (e-)')
-ax1.set_ylabel('Variance (RMS^2)')
-
-ax2.plot(signal_1e4, variance_1e5, 'bo')
-ax2.set_title('3e4 ms')
-ax2.set_xlabel('Signal (e-)')
-ax2.set_ylabel('Variance (RMS^2)')
-
 fig = plt.figure()
 ax = fig.add_subplot(111)
+
+l_init = models.Linear1D(slope=1, intercept=1)
+fit_l = fitting.LinearLSQFitter()
+l = fit_l(l_init, total_signal, total_variance)
+slope, intercept = l.slope[0], l.intercept[0]
+print "Slope, Intercept: ", slope, intercept
+
+
 plt.plot(total_signal, total_variance, 'bo')
+plt.plot(total_signal, l(total_signal), 'r')
 plt.xlabel('Signal (e-)')
 plt.ylabel('Variance (RMS^2)')
 plt.show()
